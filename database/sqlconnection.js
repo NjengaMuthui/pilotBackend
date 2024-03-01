6f606d1fd96abf33cd8bbfd5efa6837447a40fa5
Njenga Muthui <petermuthuinjenga@gmail.com>
2024-03-01 08:54:51 -0500

    Added env variables

--- database/sqlconnection.js
+++ database/sqlconnection.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 const connection = mysql
   .createPool({
-    host: "localhost",
-    user: "root",
-    password: "iliveinruiru",
-    database: "pilot"
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
   })
   .promise();
 module.exports = connection;
